8ee3944fe0898b153a6bf9ceebfb905c1a5b056a
Binbin Wu <binbin.wu@linux.intel.com>
2023-08-24 13:46:45 +0800

    bpftrace: Add three scripts
    Add three scripts for:
    - k/u function execution time
    - k/u function callstack
    - k/u function callstack count
    
    Signed-off-by: Binbin Wu <binbin.wu@linux.intel.com>

---
+++ bpftrace/stack_backtrace.sh
@@ -0,0 +1,41 @@
+usage() {
+    cat << EOM
+Usage: $(basename "$0") [OPTION]...
+  -b <user application to trace>       To specify the full path of user application
+  -u <function to trace>               Function name
+  -k <kernel function to trace>
+  -h                        Show this help
+EOM
+}
+
+
+TRACE_EVENTS="bpftrace -e '"
+
+process_args() {
+    while getopts "b:u:k:" option; do
+        case "$option" in
+            b) app=$OPTARG;;
+            u) function=$OPTARG
+               TRACE_EVENTS+="uprobe:$app:$function {print(ustack);} "
+               ;;
+            k) function=$OPTARG
+               TRACE_EVENTS+="kprobe:$function {print(kstack);} "
+               ;;
+            h) usage
+               exit 0
+               ;;
+            *)
+               echo "Invalid option '-$OPTARG'"
+               usage
+               exit 1
+               ;;
+        esac
+    done
+}
+
+
+
+process_args "$@"
+TRACE_EVENTS+="'"
+echo $TRACE_EVENTS
+eval ${TRACE_EVENTS}
